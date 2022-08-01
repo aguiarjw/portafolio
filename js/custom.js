ac5603872f7fbdd65c1ce310b67332fb1c30ba23
aguiarjw <108703050+aguiarjw@users.noreply.github.com>
2022-07-31 22:28:38 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// // concatenaar
+// console.log("Hello " + 1 + " 5");
+// let message = "ola, mundo";
+// // funcion
+// function VideosYoutube(){
+//     // variable que pega o elemento pelo su id
+//     let videoshow = document.getElementById("VideoFrance");
+//     // valor que quero colocar no elemento
+//  //   videoshow.style.display = "block";
+//     /*condicionais*/
+//     //== estamos procurando algo igual a outra
+//     // == es un valor logico
+//     // = estamos procurando algo para receber outra fonte
+//     // = o valor vai ser recibido ou sustituido por outro
+
+//     if (videoshow.style.display == "block") {
+//         videoshow.style.display = "none";
+//     }
+//     else {
+//         videoshow.style.display ="block";
+//     }
+// }
